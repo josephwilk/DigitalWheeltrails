e7412c22b940fb5029481ea180f46fd407525d50
Joseph Wilk <joe@josephwilk.net>
2022-06-16 21:07:18 +0100

    Preview screencapture

--- Assets/Scripts/Managers/ScreenCapture.cs
+++ Assets/Scripts/Managers/ScreenCapture.cs
@@ -2,9 +2,22 @@
 using System.IO;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ScreenCapture : MonoBehaviour
 {
+
+    [SerializeField]
+    private Camera screengrab= null;
+
+    [SerializeField]
+    private Image previewImage = null;
+
+    [SerializeField]
+    private GameObject imageGui = null;
+
+
     //Set your screenshot resolutions
     public int captureWidth = 1920;
     public int captureHeight = 1080;
@@ -23,6 +36,29 @@ public class ScreenCapture : MonoBehaviour
     public ScreenCapture()
     {
     }
+
+    public void Share()
+    {
+        NativeShare sharing = new NativeShare();
+        //sharing.AddFile
+    }
+
+    public void Download()
+    {
+        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(screenShot, "Wheeltrails", "screenshot.png",
+          (success, path) => Debug.Log("Media save result: " + success + " " + path));
+
+        ARDebugManager.Instance.LogInfo("SAVED");
+        Debug.Log("Permission result: " + permission);
+    }
+
+    public void CleanUp()
+    {
+        imageGui.SetActive(false);
+        previewImage.sprite = null;
+        Destroy(screenShot);
+    }
+
 
     //Initialize Directory
     private void Start()
@@ -45,93 +81,49 @@ public class ScreenCapture : MonoBehaviour
         return filename;
     }
 
-    private void CaptureScreenshot()
-    {
-        isProcessing = true;
-        // create screenshot objects
-        if (renderTexture == null)
-        {
-            // creates off-screen render texture to be rendered into
-            rect = new Rect(0, 0, captureWidth, captureHeight);
-            renderTexture = new RenderTexture(captureWidth, captureHeight, 24);
-            screenShot = new Texture2D(captureWidth, captureHeight, TextureFormat.RGB24, false);
-        }
-        // get main camera and render its output into the off-screen render texture created above
-        Camera camera = Camera.main;
-        camera.targetTexture = renderTexture;
-        camera.Render();
-        // mark the render texture as active and read the current pixel data into the Texture2D
-        RenderTexture.active = renderTexture;
-        screenShot.ReadPixels(rect, 0, 0);
-        // reset the textures and remove the render texture from the Camera since were done reading the screen data
-        camera.targetTexture = null;
-        RenderTexture.active = null;
-        // get our filename
-        string filename = CreateFileName((int)rect.width, (int)rect.height);
-        // get file header/data bytes for the specified image format
-        byte[] fileHeader = null;
-        byte[] fileData = null;
-        //Set the format and encode based on it
-        if (format == Format.RAW)
-        {
-            fileData = screenShot.GetRawTextureData();
-        }
-        else if (format == Format.PNG)
-        {
-            fileData = screenShot.EncodeToPNG();
-        }
-        else if (format == Format.JPG)
-        {
-            fileData = screenShot.EncodeToJPG();
-        }
-        else //For ppm files
-        {
-            // create a file header - ppm files
-            string headerStr = string.Format("P6\n{0} {1}\n255\n", rect.width, rect.height);
-            fileHeader = System.Text.Encoding.ASCII.GetBytes(headerStr);
-            fileData = screenShot.GetRawTextureData();
-        }
-
-        // create new thread to offload the saving from the main thread
-        new System.Threading.Thread(() =>
-        {
-            var file = System.IO.File.Create(filename);
-            if (fileHeader != null)
-            {
-                file.Write(fileHeader, 0, fileHeader.Length);
-            }
-            file.Write(fileData, 0, fileData.Length);
-            file.Close();
-            Debug.Log(string.Format("Screenshot Saved {0}, size {1}", filename, fileData.Length));
-            isProcessing = false;
-        }).Start();
-        //Cleanup
-        //Destroy(renderTexture);
-        renderTexture.Release();
-        renderTexture = null;
-        screenShot = null;
-    }
-
-
     private IEnumerator TakeScreenshotAndSave()
     {
-        yield return new WaitForEndOfFrame();
 
-        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        ss.Apply();
+        screengrab.enabled = true;
 
-        // Save the screenshot to Gallery/Photos
-        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(ss, "Wheeltrails", "screenshot.png", (success, path) => Debug.Log("Media save result: " + success + " " + path));
+        if (renderTexture == null)
+        {
+            rect = new Rect(0, 0, Screen.width, Screen.height);
+            renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+            screenShot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        }
+   
+        Camera camera = screengrab;
+        camera.targetTexture = renderTexture;
+        camera.Render();
+    
+        RenderTexture.active = renderTexture;
+        screenShot.ReadPixels(rect, 0, 0, false);
+        screenShot.Apply();
+     
+        camera.targetTexture = null;
+        RenderTexture.active = null;
+        yield return null;
 
-        Debug.Log("Permission result: " + permission);
-
+        ShowScreenshot();
+     
         // To avoid memory leaks
-        Destroy(ss);
+        Destroy(renderTexture);
+
+        screengrab.enabled = false;
     }
 
     public void TakeScreenShot()
     {
         StartCoroutine(TakeScreenshotAndSave());
+    }
+
+    public void ShowScreenshot()
+    {
+        imageGui.SetActive(true);
+        Sprite captureSprite = Sprite.Create(screenShot, new Rect(0.0f, 0.0f, screenShot.width, screenShot.height),
+    new Vector2(0.5f, 0.0f), 100.0f);
+        previewImage.sprite = captureSprite;
+
     }
 }
