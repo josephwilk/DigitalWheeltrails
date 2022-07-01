fbbc56e6164a6dde7f7cafbcb9acf7571b3ff7b4
Joseph Wilk <joe@josephwilk.net>
2022-07-01 13:48:31 +0100

    UX tweak. Dismiss on touch if interacted with.

--- Assets/Scripts/Managers/PreviewManager.cs
+++ Assets/Scripts/Managers/PreviewManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DilmerGames.UI;
 
 public class PreviewManager : MonoBehaviour
 {
@@ -22,15 +24,19 @@ public class PreviewManager : MonoBehaviour
     [SerializeField]
     private ScreenCapture screenshotManager;
 
+    private bool interacted = false;
+
     public void DownloadPreview()
     {
         screenshotManager.Download();
         disableSave();
+        interacted = true;
     }
 
     public void Share()
     {
         screenshotManager.Share();
+        interacted = true;
     }
 
 
@@ -49,10 +55,39 @@ public class PreviewManager : MonoBehaviour
     {
         saveButton.interactable = true;
         saveIcon.sprite = activeIcon;
+        interacted = false;
     }
 
     void Update()
     {
-        
+
+        if (interacted)
+        {
+#if !UNITY_EDITOR
+        OnTouch();
+#else
+            OnMouse();
+#endif
+        }
+    }
+
+    void OnTouch()
+    {
+        if (Input.touchCount > 0 &&
+    Input.GetTouch(0).phase == TouchPhase.Began &&
+    EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+        {
+            gameObject.GetComponent<UIPane>().Hide();
+        }
+
+    }
+
+    private void OnMouse()
+    {
+        if (Input.GetMouseButton(0) &&
+            EventSystem.current.IsPointerOverGameObject())
+        {
+            gameObject.GetComponent<UIPane>().Hide();
+        }
     }
 }
